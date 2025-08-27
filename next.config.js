584a669f7af6229933538b39b5408af96c365c4e
Jimmy Fellous <jimmyfellous@gmail.com>
2025-08-27 22:38:50 +0200

    Revert to simple config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  // Configuration standard pour Vercel
 }
 
 module.exports = nextConfig 
